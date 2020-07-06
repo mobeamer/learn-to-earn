44c6a17f8a47cb382de665c11609d4d3f6929f75
Markus Beamer <MarkusB@rtriad.pro>
2020-07-06 14:00:39 -0400

    changed

--- landing/routes/index.js
+++ landing/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Worlddddddd' });
+  res.render('index', { title: 'Hello Worldddd' });
 });
 
 module.exports = router;
